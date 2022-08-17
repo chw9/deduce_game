681a6dfd95b9bc8478fb2c9016b3b9ec790d68d4
Caroline Winakur <caroline.winakur@duke.edu>
2022-08-17 16:15:43 -0400

    functional for single-player version

--- deduce.py
+++ deduce.py
@@ -1,5 +1,6 @@
 import random
 from os.path import exists
+from readline import get_history_length
 
 class Card:
     def __init__(self, value, suit):
@@ -10,6 +11,13 @@ class Card:
         if self.val == 1:
             return f"A of {self.suit}"
         return f"{self.val} of {self.suit}"
+    
+    def __eq__(self, other): 
+        if not isinstance(other, Card):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self.val == other.val and self.suit == other.suit
 
 numbers = (1, 2, 3, 4, 5, 6)
 suits = ("spades", "hearts", "clubs")
@@ -57,7 +65,7 @@ def deal(deck):
     p2 = []
     p3 = []
     e = []
-    w = int
+    w = Card
     
     for i in range(len(deck)):
         if i == len(deck) - 1 or i == len(deck) - 3:
@@ -94,18 +102,21 @@ def deal(deck):
 
     return p1, p2, p3, e, w, m
 
-# player = 1, 2, or 3 depending on whose turn it is
 def show_q_cards(q_deck_remaining):
+    if len(q_deck_remaining) == 0:
+        q_deck_remaining = shuffle(copy_deck(q_deck))
+
     three_q_cards = (q_deck_remaining[0], q_deck_remaining[1], q_deck_remaining[2])
     for i in range(3):
         q_deck_remaining.remove(q_deck_remaining[0])
     return three_q_cards
 
 # TODO: maybe add "confirm selection? y/n"
+# player = 1, 2, or 3 depending on whose turn it is; default is 1
 def get_q_selection(three_cards, player=1):
-    print(f"card options: 1.) {three_cards[0]}, 2.) {three_cards[1]}, 3.) {three_cards[2]} ")
-    print("remember: order matters! range will be determined in order of selection.")
-    choice = input("select two cards for your question by typing two numbers (no spaces or punctuation): ")
+    print(f"  Card options: 1.) {three_cards[0]}, 2.) {three_cards[1]}, 3.) {three_cards[2]} ")
+    print("  Remember: order matters! Range will be determined in order of selection.")
+    choice = input("> Select two cards for your question by typing two numbers (no spaces or punctuation): ")
     c1 = int(choice[0:1]) - 1
     c2 = int(choice[1]) - 1
 
@@ -113,7 +124,7 @@ def get_q_selection(three_cards, player=1):
 
     p = list(set((1, 2, 3)) - set([player]))
 
-    player = input(f"would you like to interrogate player {p[0]} or player {p[1]}? ")
+    player = input(f"\nWould you like to interrogate player {p[0]} or player {p[1]}? ({p[0]}/{p[1]}): ")
 
     return selected, player
 
@@ -162,9 +173,42 @@ def parse_question(two_cards, player):
                 if card.val >= c1.val or card.val <= c2.val:
                     a=a+1
 
-    print(f"@ player {player}: {q}")
+    print(f"    @ player {player}: {q}")
     s = "" if a == 1 else "s"
-    print(f"player {player} has {a} card{s} in this range")
+    print(f"> Player {player} has {a} card{s} in this range")
+
+def accusation(player=1):
+    if (player == 1):
+        guess = input("\nWhich player is the murderer? (2 or 3): ")
+        player_hand = p1
+    elif (player == 2):
+        guess = input("\nWhich player is the murderer? (1 or 3): ")
+        player_hand = p2
+    else:
+        guess = input("\nWhich player is the murderer? (1 or 2): ")
+        player_hand = p3
+
+
+    hand = p1 if guess == "1" else p2 if guess == "2" else p3
+    one_higher = Card(murderer.val + 1, murderer.suit)
+
+# TODO: account for if one higher is also in the player's hand
+    if murderer in hand or (murderer in player_hand and one_higher in hand) or (murderer == w and one_higher in hand):
+        evidence_guess_one = input("What do you think the first evidence card is? Order doesn't matter.\nFormat as [number 1-6][suit: c, h, s]. eg 3h is 3 of hearts.\nYour guess: ")
+        evidence_guess_two = input("What do you think the second evidence card is? Order doesn't matter.\nFormat as [number 1-6][suit: c, h, s]. eg 3h is 3 of hearts.\nYour guess: ")
+
+        suit1 = "clubs" if evidence_guess_one[1] == "c" else "hearts" if evidence_guess_one[1] == "h" else "spades"
+        suit2 = "clubs" if evidence_guess_two[1] == "c" else "hearts" if evidence_guess_two[1] == "h" else "spades"
+
+        e1 = Card(int(evidence_guess_one[0:1]), suit1)
+        e2 = Card(int(evidence_guess_two[0:1]), suit2)
+
+        if (e1 == e[0] and e2 == e[1]) or (e2 == e[0] and e1 == e[1]):
+            print("Congratulations, you win!")
+        else:
+            print("Sorry, that's not correct... look at dontread.txt for the answer.")
+    else:
+        print("Sorry, that's not correct... look at dontread.txt for the answer.")
 
 
 ################################## gameplay: ##################################
@@ -172,23 +216,25 @@ def parse_question(two_cards, player):
 shuffle(deck)
 shuffle(q_deck_remaining)
 p1, p2, p3, e, w, murderer = deal(deck)
-# print(e[0])
-# print(e[1])
-# print(murderer)
-a = show_q_cards(q_deck_remaining)
-# for i in range(len(a)):
-#     print(a[i])
 
-sel_cards, p = get_q_selection(a)
-# for i in range(len(sel_cards)):
-#     print(sel_cards[i])
-# print(p)
+playing = True
+q_num = 1
 
-for i in range(len(p2)):
-    print(p2[i])
+print(f"\nQuestion {q_num}: ")
 
-parse_question(sel_cards, p)
+while playing:
+    a = show_q_cards(q_deck_remaining)
+    sel_cards, p = get_q_selection(a)
+    parse_question(sel_cards, p)
+    guess = input("\nWould you like to make an accusation (y), continue (n), or quit (q)? (y/n/q): ")
 
+    if guess == "y":
+        accusation()
+        playing = False
+    elif guess == "q":
+        print("\nOkay")
+        playing = False
+    else:
+        q_num = q_num + 1
+        print(f"\nQuestion {q_num}: ")
 
-# for i in range(len(deck)):
-#     print(deck[i])
